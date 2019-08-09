7f3987cdd8a6c4b15cdcbe22da05a3ee735525a5
Francinildo Rodrigues <francis584@gmail.com>
2019-08-09 15:27:15 -0300

    style repository.comp - done

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import Container from '../../components/Container';
+import { Loading, Owner } from './styles';
 
 export default class Repository extends Component {
   state = {
@@ -43,7 +45,21 @@ export default class Repository extends Component {
   }
 
   render() {
-    const { match } = this.props;
-    return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+    const { repository, issues, loading } = this.state;
+
+    if (loading) {
+      return <Loading>Carregando...</Loading>;
+    }
+
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Voltar</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+      </Container>
+    );
   }
 }
